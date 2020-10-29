5628884d5420c2e66ec220dfe30ec3d8cccd57bf
javed0863 <javed0863@gmail.com>
2020-10-29 21:49:21 +0800

    Updated logic to get base url from request

--- src/main/java/com/javedrpi/xiaourl/controller/LittleUrlController.java
+++ src/main/java/com/javedrpi/xiaourl/controller/LittleUrlController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.view.RedirectView;
 
+import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
@@ -32,22 +33,29 @@ public class LittleUrlController {
     @Autowired
     private UrlService urlService;
 
-    @Value("${xiao.url.base}")
-    private String BASE_XIAOURL;
-
     @GetMapping("/xiao")
-    public ResponseEntity<UrlsDto> xiaoUrl(@RequestParam("url") final String url){
+    public ResponseEntity<UrlsDto> xiaoUrl(@RequestParam("url") final String url,
+                                           HttpServletRequest request){
+        String baseurl = getBaseShortUrl(request);
+
         String urlCode = uniqueRandomService.getUniqueRandom(getSeedvalue());
-        String shortenUrl = BASE_XIAOURL.concat(urlCode);
+        String shortenUrl = baseurl.concat(urlCode);
         Urls savedRecord = urlService.insertRecord(url, shortenUrl);
         UrlsDto urlsDto = new UrlsDto(savedRecord.getOriginalUrl(), savedRecord.getShortenUrl());
+
         return new ResponseEntity<>(urlsDto, HttpStatus.OK);
     }
 
+    private String getBaseShortUrl(HttpServletRequest request) {
+        String baseurl = request.getRequestURL().toString();
+        return baseurl.replace("xiao","lu/");
+    }
+
     @GetMapping("/lu/{urlcode}")
-    public RedirectView redirectToOriginal(@PathVariable("urlcode") final String urlcode){
+    public RedirectView redirectToOriginal(@PathVariable("urlcode") final String urlcode,
+                                           HttpServletRequest request){
         logger.info("redirecting...");
-        Urls savedRecord = urlService.findOriginalUrl(BASE_XIAOURL.concat(urlcode));
+        Urls savedRecord = urlService.findOriginalUrl(request.getRequestURL().toString());
         return new RedirectView(savedRecord.getOriginalUrl());
     }
 
